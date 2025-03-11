b8adda1178cf1157474db1fc9df9fe4034ea27ba
apatankars <armaan_patankar@brown.edu>
2025-03-10 21:12:22 -0400

    Updated Navigation Bar and submenus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import NavigationBar from "./components/NavigationBar";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
 
 export default function Home() {
   return (
